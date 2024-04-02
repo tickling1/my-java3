f9141bac0fa52fac73aef2dce6325696c5c0304f
tickling1 <allworldreview0130@gmail.com>
2024-04-02 17:27:32 +0900

    Java-04-02

--- lang/object/equals/EqualsMainV2.java
+++ lang/object/equals/EqualsMainV2.java
@@ -7,6 +7,6 @@ public class EqualsMainV2 {
         UserV2 user2 = new UserV2("id-100");
 
         System.out.println("identity = " + (user1 == user2));
-        System.out.println("equality = " + (user1.equals(user2)));
+        System.out.println("equality = " + (user1.equals(user2)));statu
     }
 }
